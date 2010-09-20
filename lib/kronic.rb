2934c77498a756d19976298f8b581cd8908f8fcc
Xavier Shay <xavier@rhnh.net>
2010-09-20 13:39:06 +0100

    Refactor, extract method

--- lib/kronic.rb
+++ lib/kronic.rb
@@ -11,40 +11,9 @@ class Kronic
     string = string.to_s.downcase.strip
     today  = Date.today
 
-    return Date.today     if string == 'today'
-    return Date.yesterday if string == 'yesterday'
-    return Date.tomorrow  if string == 'tomorrow'
-
-    tokens = string.split(/\s+/)
-
-    # Last|This X
-    if %w(last this).include?(tokens[0])
-      days = (1..7).map {|x| 
-        Date.today + (tokens[0] == 'last' ? -x.days : x.days)
-      }.inject({}) {|a, x| 
-        a.update(x.strftime("%A").downcase => x) 
-      }
-      return days[tokens[1]]
-    end
-
-    # 14 Sep, 14 September, 14 September 2010
-    if tokens[0] =~ /^[0-9]+$/ && tokens[1]
-      day   = tokens[0].to_i
-      month = month_from_name(tokens[1])
-      year  = if tokens[2]
-        tokens[2] =~ /^[0-9]+$/ ? tokens[2].to_i : nil
-      else
-        today.year
-      end
-
-      return nil unless day && month && year
-
-      result = Date.new(year, month, day)
-      result -= 1.year if result > Date.today
-      return result
-    end
-
-    nil
+    parse_nearby_days(string, today) ||
+      parse_last_or_this_day(string, today) ||
+      parse_exact_date(string, today)
   end
 
   # Converts a date to a human readable string.
@@ -59,11 +28,55 @@ class Kronic
     end
   end
 
-  def self.month_from_name(month)
-    return nil unless month
+  class << self
+    private
 
-    human_month = month.downcase.humanize
-    Date::MONTHNAMES.index(human_month) || Date::ABBR_MONTHNAMES.index(human_month)
+    def month_from_name(month)
+      return nil unless month
+
+      human_month = month.downcase.humanize
+      Date::MONTHNAMES.index(human_month) || Date::ABBR_MONTHNAMES.index(human_month)
+    end
+
+    def parse_nearby_days(string, today)
+      return today         if string == 'today'
+      return today - 1.day if string == 'yesterday'
+      return today + 1.day if string == 'tomorrow'
+    end
+
+    def parse_last_or_this_day(string, today)
+      tokens = string.split(/\s+/)
+
+      if %w(last this).include?(tokens[0])
+        days = (1..7).map {|x| 
+          today + (tokens[0] == 'last' ? -x.days : x.days)
+        }.inject({}) {|a, x| 
+          a.update(x.strftime("%A").downcase => x) 
+        }
+
+        days[tokens[1]]
+      end
+    end
+
+    def parse_exact_date(string, today)
+      tokens = string.split(/\s+/)
+
+      # 14 Sep, 14 September, 14 September 2010
+      if tokens[0] =~ /^[0-9]+$/ && tokens[1]
+        day   = tokens[0].to_i
+        month = month_from_name(tokens[1])
+        year  = if tokens[2]
+          tokens[2] =~ /^[0-9]+$/ ? tokens[2].to_i : nil
+        else
+          today.year
+        end
+
+        return nil unless day && month && year
+
+        result = Date.new(year, month, day)
+        result -= 1.year if result > today
+        result
+      end
+    end
   end
-  private_class_method :month_from_name
 end
